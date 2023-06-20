aa35fcd87762987864d425d6b243f549306ddaf8
Bakhtiar <bakhtiarsahib0@gail.com>
2023-06-20 00:17:52 -0700

    Area Calculator 20/06/23

---
+++ AreaCalculator/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+app.get('/',(req,res)=>{
+res.sendFile(__dirname + "/index.html");
+})
+app.get('/circle',(req,res)=>{
+    res.sendFile(__dirname + "/circle.html");
+    })
+
+    app.get('/triangle',(req,res)=>{
+        res.sendFile(__dirname + "/triangle.html");
+        })
+
+
+
+        app.post('/triangle',(req,res)=>{
+            const height = req.body.height;
+            const base = req.body.base;
+            const area = 0.5 * base * height ;
+            res.send(`<h2>Area of triangle is ${area}</h2>`); 
+            })
+
+            app.post('/circle',(req,res)=>{
+                const radius = req.body.radius;
+                const area = 3.1416 * radius * radius ;
+                res.send(`<h2>Area of Circle is ${area}</h2>`); 
+                })
+              
+        
+        
+    
+
+app.listen(PORT,()=>{
+    console.log(`server is running at http://localhost:${PORT}`);
+})
